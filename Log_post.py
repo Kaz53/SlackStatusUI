2d9189efe2f4c31993512e84047f04cf4392eebd
Kaz53 <kishikawa@fxpal.com>
2019-09-20 20:13:25 -0700

    Modified post log scheme.

--- Log_post.py
+++ Log_post.py
@@ -15,7 +15,6 @@ if __name__ == '__main__':
     if platform.system() == "Linux":
         os.chdir('/home/pi/Projects/SlackStatusUI')
     pdirname = os.getcwd()
-    dirname = os.path.join(pdirname, "data")
 
     # Slack Seetings
     Slack_conf_file = os.path.join(pdirname, 'Slack_conf.json')
@@ -24,10 +23,9 @@ if __name__ == '__main__':
     slack_kazu_url = Slack_conf_json['Slack_Kazu_channel']
 
     # Read error log
-    error_log_file = open(os.path.join(pdirname, 'log.txt'))
-    if os.path.exists(error_log_file):
-        error_log = error_log_file.read()
-        error_log_file.close()
+    error_log_file = open(os.path.join(pdirname, 'log/log.txt'))
+    error_log = error_log_file.read()
+    error_log_file.close()
 
-        if error_log != "":
-            post_slack(error_log)
+    if error_log != "":
+        post_slack(error_log)
